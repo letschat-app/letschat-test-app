ef9e3b3242e461b56458ef0943e45eb55fc52b61
Yokeshwar-intern <intern1@intern.imaggar.com>
2026-09-10 12:52:28 +0530

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/letschat-test-app/service-worker.js', { scope: '/letschat-test-app/' })})}
